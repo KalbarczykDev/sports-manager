4221e9fa0e4a3e362be7553778c32ae9a6f0c7a3
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-07-25 12:29:12 +0200

    migrate to spring boot

---
+++ src/main/java/dev/kalbarczyk/sportsmanager/SportsManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.kalbarczyk.sportsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kalbarczyk/sportsmanager/SportsmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kalbarczyk.sportsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
