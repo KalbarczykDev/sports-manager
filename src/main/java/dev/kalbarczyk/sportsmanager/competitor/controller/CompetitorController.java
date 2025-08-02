5ae0f7fd8d4ee78ca77dc134ea59b181ddc59b04
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-08-02 17:30:25 +0200

    combine controllers

--- src/main/java/dev/kalbarczyk/sportsmanager/competitor/controller/CompetitorController.java
+++ src/main/java/dev/kalbarczyk/sportsmanager/competitor/controller/CompetitorController.java
@@ -1,12 +1,13 @@
 package dev.kalbarczyk.sportsmanager.competitor.controller;
 
+import dev.kalbarczyk.sportsmanager.competitor.model.Competitor;
 import dev.kalbarczyk.sportsmanager.competitor.service.CompetitorService;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/competitors")
 @Controller
@@ -26,5 +27,32 @@ public class CompetitorController {
         return "competitor/index";
     }
 
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Competitor getCompetitorById(final @PathVariable Long id) {
+        return competitorService.findById(id);
+    }
+
+    @PostMapping
+    @ResponseBody
+    public Competitor createCompetitor(@RequestBody Competitor competitor) {
+        return competitorService.save(competitor);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<Void> deleteCompetitor(final @PathVariable Long id) {
+        System.out.println("Deleting competitor with ID: " + id);
+        competitorService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    @ResponseBody
+    public Competitor updateCompetitor(final @PathVariable Long id, final @RequestBody Competitor competitor
+    ) {
+        return competitorService.update(id, competitor);
+    }
 
 }
