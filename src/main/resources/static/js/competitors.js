a86eec316eb94be14621b9776630c23a2299e9bc
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-08-04 18:33:20 +0200

    competitors list css and frontend refactor

---
+++ src/main/resources/static/js/competitors.js
@@ -0,0 +1,52 @@
+let competitorToDeleteId = null;
+const deleteModal = document.getElementById('deleteModal');
+const confirmDeleteBtn = document.getElementById('confirmDeleteBtn');
+const cancelDeleteBtn = document.getElementById('cancelDeleteBtn');
+
+
+function showDeleteModal(id) {
+    competitorToDeleteId = id;
+    deleteModal.style.display = 'flex';
+}
+
+
+function hideDeleteModal() {
+    deleteModal.style.display = 'none';
+    competitorToDeleteId = null;
+}
+
+confirmDeleteBtn.addEventListener('click', () => {
+    if (competitorToDeleteId) {
+        fetch('/competitors/' + competitorToDeleteId, {
+            method: 'DELETE'
+        })
+            .then(response => {
+                if (response.ok) {
+                    console.log('Competitor deleted successfully!');
+                    window.location.reload();
+                } else {
+                    console.error('Error deleting Competitor.');
+
+                }
+            })
+            .catch(error => {
+                console.error('Network error:', error);
+
+            })
+            .finally(() => {
+                hideDeleteModal();
+            });
+    }
+});
+
+
+cancelDeleteBtn.addEventListener('click', () => {
+    hideDeleteModal();
+});
+
+
+deleteModal.addEventListener('click', (event) => {
+    if (event.target === deleteModal) {
+        hideDeleteModal();
+    }
+});
