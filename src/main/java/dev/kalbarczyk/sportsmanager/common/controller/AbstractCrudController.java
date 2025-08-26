0247ded8a9e292ad431056d079e3bd731e166437
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-08-26 15:03:16 +0200

    base controller abstraction

---
+++ src/main/java/dev/kalbarczyk/sportsmanager/common/controller/AbstractCrudController.java
@@ -0,0 +1,97 @@
+package dev.kalbarczyk.sportsmanager.common.controller;
+
+import dev.kalbarczyk.sportsmanager.common.model.BaseEntity;
+import dev.kalbarczyk.sportsmanager.common.service.BaseService;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+public abstract class AbstractCrudController<T extends BaseEntity> {
+
+    protected abstract BaseService<T> getBaseService();
+
+    protected abstract String getModuleName();
+
+    protected abstract void prepareFormModel(Model model, T entity, String formAction, String title);
+
+    protected abstract void validateEntity(T entity, BindingResult bindingResult);
+
+    protected abstract T createNewInstance();
+
+
+    @GetMapping
+    public String index(
+            final Model model,
+            @RequestParam(name = "sortBy", defaultValue = "id") String sortBy,
+            @RequestParam(name = "sortDir", defaultValue = "asc") String sortDir
+    ) {
+        log.info("Fetching all {} sorted by {} {}", getModuleName(), sortBy, sortDir);
+        val entities = getBaseService().findAll(sortBy, sortDir);
+        model.addAttribute(getModuleName() + "s", entities);
+        model.addAttribute("sortBy", sortBy);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("view", "modules/" + getModuleName() + "/index");
+        return "layout/layout";
+    }
+
+    @GetMapping("{id}")
+    public String show(final @PathVariable Long id, final Model model) {
+        log.info("Fetching {} with id {}", getModuleName(), id);
+        val entity = getBaseService().findById(id);
+        model.addAttribute(getModuleName(), entity);
+        model.addAttribute("view", "modules/" + getModuleName() + "/show");
+        return "layout/layout";
+    }
+
+    @PostMapping({"", "/{id}"})
+    public String save(
+            @PathVariable(required = false) Long id,
+            @ModelAttribute T entity,
+            BindingResult bindingResult,
+            Model model) {
+
+        validateEntity(entity, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            String formAction = (id == null) ? "/" + getModuleName() + "s" : "/" + getModuleName() + "s/" + id;
+            String title = (id == null ? "New " : "Edit ") + getModuleName();
+            prepareFormModel(model, entity, formAction, title);
+            return "layout/layout";
+        }
+
+        if (id == null) {
+            getBaseService().save(entity);
+        } else {
+            getBaseService().update(id, entity);
+        }
+
+        return "redirect:/" + getModuleName() + "s";
+    }
+
+    @GetMapping("/new")
+    public String showNewForm(Model model) {
+        prepareFormModel(model, createNewInstance(), "/" + getModuleName() + "s", "New " + getModuleName());
+        return "layout/layout";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String showEditForm(@PathVariable Long id, Model model) {
+        prepareFormModel(model,
+                getBaseService().findById(id),
+                "/" + getModuleName() + "s/" + id,
+                "Edit " + getModuleName());
+        return "layout/layout";
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        log.info("Deleting {} with id {}", getModuleName(), id);
+        getBaseService().delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/dev/kalbarczyk/sportsmanager/competitor/controller/CompetitorController.java
+++ src/main/java/dev/kalbarczyk/sportsmanager/competitor/controller/CompetitorController.java
@@ -1,22 +1,23 @@
 package dev.kalbarczyk.sportsmanager.competitor.controller;
 
+import dev.kalbarczyk.sportsmanager.common.controller.AbstractCrudController;
+import dev.kalbarczyk.sportsmanager.common.service.BaseService;
 import dev.kalbarczyk.sportsmanager.common.service.CountryService;
 import dev.kalbarczyk.sportsmanager.competitor.model.Competitor;
 import dev.kalbarczyk.sportsmanager.competitor.service.CompetitorService;
+import dev.kalbarczyk.sportsmanager.person.enums.Discipline;
 import dev.kalbarczyk.sportsmanager.person.validation.PersonValidator;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @RequestMapping("/competitors")
 @Controller
-public class CompetitorController {
+public class CompetitorController extends AbstractCrudController<Competitor> {
 
     private final CompetitorService competitorService;
     private final PersonValidator competitorValidator;
@@ -32,94 +33,33 @@ public class CompetitorController {
         this.countryService = countryService;
     }
 
-    @GetMapping
-    public String index(
-            final Model model,
-            final @RequestParam(name = "sortBy", defaultValue = "id") String sortBy,
-            final @RequestParam(name = "sortDir", defaultValue = "asc") String sortDir
-    ) {
-        log.info("Received request to get all competitors with sorting: sortBy={}, sortDir={}", sortBy, sortDir);
-
-        val competitors = competitorService.findAll(sortBy, sortDir);
-        model.addAttribute("competitors", competitors);
-        model.addAttribute("sortBy", sortBy);
-        model.addAttribute("sortDir", sortDir);
-        model.addAttribute("view", "modules/competitor/index");
-        return "layout/layout";
-    }
-
-    @GetMapping("{id}")
-    public String show(final @PathVariable Long id, final Model model) {
-        log.info("Received request to get competitors of id: {}", id);
-
-        val competitor = competitorService.findById(id);
-        model.addAttribute("competitor", competitor);
-        model.addAttribute("view", "modules/competitor/show");
-        return "layout/layout";
-    }
-
-    @GetMapping("/new")
-    public String showNewForm(final Model model) {
-        log.info("Received request to show add competitor form");
-        prepareFormModel(model, new Competitor(),
-                "/competitors", "New competitor");
-        return "layout/layout";
-    }
-
-    @GetMapping("/{id}/edit")
-    public String showEditForm(final @PathVariable Long id, final Model model) {
-        log.info("Received request to edit competitor with id: {}", id);
-        prepareFormModel(model, competitorService.findById(id),
-                "/competitors/" + id, "Edit competitor");
-        return "layout/layout";
-    }
-
-    @PostMapping({"", "/{id}"})
-    public String saveCompetitor(@PathVariable(required = false) Long id,
-                                 @ModelAttribute Competitor competitor,
-                                 BindingResult bindingResult,
-                                 Model model) {
-        competitorValidator.validate(competitor, bindingResult);
-
-        if (bindingResult.hasErrors()) {
-
-            final String uri;
-            final String title;
-
-            if (id == null) {
-                title = "New competitor";
-                uri = "/competitors";
-            } else {
-                title = "Edit competitor";
-                uri = "/competitors/" + id;
-            }
-            prepareFormModel(model, competitor, uri, title);
-            return "layout/layout";
-        }
-
-        if (id == null) {
-            competitorService.save(competitor);
-        } else {
-            competitorService.update(id, competitor);
-        }
-
-        return "redirect:/competitors";
-    }
-
-    private void prepareFormModel(Model model, Competitor competitor, String formAction, String title) {
+    @Override
+    protected void prepareFormModel(Model model, Competitor competitor, String formAction, String title) {
         model.addAttribute("competitor", competitor);
         model.addAttribute("countries", countryService.getCountriesForForm());
+        model.addAttribute("disciplines", Discipline.values());
         model.addAttribute("formAction", formAction);
         model.addAttribute("title", title);
         model.addAttribute("view", "modules/competitor/form");
     }
 
-    @DeleteMapping("/{id}")
-    @ResponseBody
-    public ResponseEntity<Void> deleteCompetitor(final @PathVariable Long id) {
-        log.info("Received request to delete competitor via API with id: {}", id);
-        competitorService.delete(id);
-        return ResponseEntity.noContent().build();
+    @Override
+    protected void validateEntity(Competitor entity, BindingResult bindingResult) {
+        competitorValidator.validate(entity, bindingResult);
     }
 
+    @Override
+    protected Competitor createNewInstance() {
+        return new Competitor();
+    }
+
+    @Override
+    protected BaseService<Competitor> getBaseService() {
+        return competitorService;
+    }
+
+    @Override
+    protected String getModuleName() {
+        return "competitor";
+    }
 }
