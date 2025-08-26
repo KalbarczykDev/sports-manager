2de1469da8c0ef2d91fe9cfab2381ec1d557ee0d
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-08-26 14:36:23 +0200

    coach service

--- src/main/java/dev/kalbarczyk/sportsmanager/coach/service/CoachService.java
+++ src/main/java/dev/kalbarczyk/sportsmanager/coach/service/CoachService.java
@@ -1,18 +1,8 @@
 package dev.kalbarczyk.sportsmanager.coach.service;
 
 import dev.kalbarczyk.sportsmanager.coach.model.Coach;
-import dev.kalbarczyk.sportsmanager.competitor.model.Competitor;
+import dev.kalbarczyk.sportsmanager.common.service.BaseService;
 
-import java.util.List;
+public interface CoachService extends BaseService<Coach> {
 
-public interface CoachService {
-    List<Coach> findAll(final String sortBy, final String sortDir);
-
-    Coach findById(final Long id);
-
-    Coach save(final Competitor competitor);
-
-    Coach update(final Long id, final Coach coach);
-
-    void delete(final Long id);
 }
--- src/main/java/dev/kalbarczyk/sportsmanager/coach/service/DefaultCoachService.java
+++ src/main/java/dev/kalbarczyk/sportsmanager/coach/service/DefaultCoachService.java
@@ -2,21 +2,16 @@ package dev.kalbarczyk.sportsmanager.coach.service;
 
 import dev.kalbarczyk.sportsmanager.coach.model.Coach;
 import dev.kalbarczyk.sportsmanager.coach.repository.CoachRepository;
-import dev.kalbarczyk.sportsmanager.common.exception.CrudException;
-import dev.kalbarczyk.sportsmanager.competitor.model.Competitor;
+import dev.kalbarczyk.sportsmanager.common.service.AbstractBaseService;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.mapping.PropertyReferenceException;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Slf4j
 @Service
 @Primary
-public class DefaultCoachService implements CoachService {
+public class DefaultCoachService extends AbstractBaseService<Coach> implements CoachService {
 
     private final CoachRepository coachRepository;
 
@@ -24,34 +19,9 @@ public class DefaultCoachService implements CoachService {
         this.coachRepository = coachRepository;
     }
 
-    @Override
-    public List<Coach> findAll(String sortBy, String sortDir) {
-        log.info("Fetching all coaches");
-        try {
-            val sort = Sort.by(Sort.Direction.fromString(sortDir), sortBy);
-            return coachRepository.findAll(sort);
-        } catch (PropertyReferenceException e) {
-            throw new CrudException.InvalidSortingArgument(e.getMessage());
-        }
-    }
 
     @Override
-    public Coach findById(Long id) {
-        return null;
-    }
-
-    @Override
-    public Coach save(Competitor competitor) {
-        return null;
-    }
-
-    @Override
-    public Coach update(Long id, Coach coach) {
-        return null;
-    }
-
-    @Override
-    public void delete(Long id) {
-
+    protected JpaRepository<Coach, Long> getRepository() {
+        return coachRepository;
     }
 }
