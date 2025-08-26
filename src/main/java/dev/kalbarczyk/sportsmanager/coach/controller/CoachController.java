4c59d50183ab158d10026b0dbc976f086e93bc6a
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-08-26 17:04:01 +0200

    coach list view

--- src/main/java/dev/kalbarczyk/sportsmanager/coach/controller/CoachController.java
+++ src/main/java/dev/kalbarczyk/sportsmanager/coach/controller/CoachController.java
@@ -1,30 +1,56 @@
 package dev.kalbarczyk.sportsmanager.coach.controller;
 
+import dev.kalbarczyk.sportsmanager.coach.model.Coach;
 import dev.kalbarczyk.sportsmanager.coach.service.DefaultCoachService;
+import dev.kalbarczyk.sportsmanager.common.controller.AbstractCrudController;
+import dev.kalbarczyk.sportsmanager.common.service.BaseService;
+import dev.kalbarczyk.sportsmanager.person.enums.Discipline;
+import dev.kalbarczyk.sportsmanager.person.validation.PersonValidator;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @RequestMapping("/coaches")
 @Controller
-public class CoachController {
+public class CoachController extends AbstractCrudController<Coach> {
     private final DefaultCoachService coachService;
+    private final PersonValidator personValidator;
 
-    @Autowired
-    public CoachController(final DefaultCoachService coachService) {
+    public CoachController(final DefaultCoachService coachService, final PersonValidator personValidator) {
         this.coachService = coachService;
+        this.personValidator = personValidator;
     }
 
-    @GetMapping
-    public String index(final Model model) {
 
-        val coaches = coachService;
+    @Override
+    protected BaseService<Coach> getBaseService() {
+        return coachService;
+    }
 
-        return "layout/layout";
+    @Override
+    protected String getModuleName() {
+        return "coach";
+    }
+
+    @Override
+    protected void prepareFormModel(Model model, Coach entity, String formAction, String title) {
+        model.addAttribute("coach", entity);
+        model.addAttribute("formAction", formAction);
+        model.addAttribute("title", title);
+        model.addAttribute("disciplines", Discipline.values());
+        model.addAttribute("view", "modules/coach/form");
+    }
+
+    @Override
+    protected void validateEntity(Coach entity, BindingResult bindingResult) {
+        personValidator.validate(entity, bindingResult);
+    }
+
+    @Override
+    protected Coach createNewInstance() {
+        return new Coach();
     }
 }
--- src/main/java/dev/kalbarczyk/sportsmanager/coach/service/CoachService.java
+++ src/main/java/dev/kalbarczyk/sportsmanager/coach/service/CoachService.java
@@ -4,5 +4,4 @@ import dev.kalbarczyk.sportsmanager.coach.model.Coach;
 import dev.kalbarczyk.sportsmanager.common.service.BaseService;
 
 public interface CoachService extends BaseService<Coach> {
-
 }
